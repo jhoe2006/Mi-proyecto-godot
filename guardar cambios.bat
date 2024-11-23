d634fd005546cce8bb3692815a99d8fa170cc95e
juanperezma <146695529+juanperezma@users.noreply.github.com>
2024-11-22 21:59:08 -0500

    Cambios guardados

--- guardar cambios.bat
+++ guardar cambios.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d "C:\Users\jhoec\Documents\GitHub\MiProyectoGodot"  REM
+cd /d "C:\Users\jhoec\Documents\GitHub\Mi-proyecto-godot"  REM
 git add .
 git commit -m "Cambios guardados"
 git push origin main
